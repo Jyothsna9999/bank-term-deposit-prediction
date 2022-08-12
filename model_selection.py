9984006a0b0a36284640747cd7fe48d5e3599916
Jyothsna9999 <105665059+Jyothsna9999@users.noreply.github.com>
2022-08-12 15:26:42 +0530

    Update model_selection.py

--- model_selection.py
+++ model_selection.py
@@ -10,7 +10,7 @@ def model_selection():
     data = pd.read_csv('balanced_data.csv')
     X = data.drop(['y_new'],axis=1)
     y = data['y_new']
-    X_train, y_train, X_test, y_test = train_test_split(X,y,test_size=0.2, random_state=0) 
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0) 
     dt = DecisionTreeClassifier()
     dt.fit(X_train, y_train)
     print(dt.score(X_test, y_test))
